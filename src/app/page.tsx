24b92e6f971f257fdae7d4657c751b44f570a9ca
bendechrai <ben@dechrai.com>
2025-05-15 12:10:51 -0500

    Make sure page uses consistent redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,15 @@ export default function Home() {
   const { isConfigured } = useConfig();
   const router = useRouter();
 
-  // If not configured, show the configuration wizard
-  if (!isConfigured) {
-    return <Config />;
-  }
-
-  // Redirect to the list of buckets when configuration is complete
-  
   useEffect(() => {
     if (isConfigured) {
       router.push('/buckets');
     }
   }, [isConfigured, router]);
   
-  return null; // This will briefly show before redirect happens
+  if (!isConfigured) {
+    return <Config />;
+  }
+  
+  return null;
 }
